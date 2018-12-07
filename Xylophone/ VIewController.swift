2451ec7c2c6d941b7a08e109b1ebcd279f23cfc3
Monk <monk@industriallogic.com>
2018-12-07 13:39:17 -0800

    Finish buttons playing different sounds based on tags

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,23 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
-    
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+
+    var player: AVAudioPlayer!
+    let soundArray: [String] = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
-
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        
-        
+        let selectedSoundFileName: String = soundArray[sender.tag - 1]
+        playSound(soundFile: selectedSoundFileName)
     }
-    
-  
 
+    func playSound(soundFile: String) {
+        let soundFile = Bundle.main.url(forResource: soundFile, withExtension: "wav")!
+        do {
+            player = try AVAudioPlayer(contentsOf: soundFile)
+            guard player != nil else {
+                return
+            }
+            player.prepareToPlay()
+        } catch let error {
+            print(error)
+        }
+        player.play()
+    }
 }
 
